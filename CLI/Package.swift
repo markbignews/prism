73a8eae75203cae9fc0913d851363a8070dd9237
noneedloong <noneedloong@users.noreply.github.com>
2026-07-04 15:36:43 +0800

    Switch to Apple scrollPosition API for chat auto-scroll

--- CLI/Package.swift
+++ CLI/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "PrismCLI",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v15),
     ],
     products: [
         .executable(name: "prism", targets: ["PrismCLI"]),
